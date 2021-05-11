4f7fb68d044a0cb33eba17dcbde0336d7713a67a
jonacappelle <jonacappelle@hotmail.com>
2021-05-11 10:42:43 +0200

    Add APP ERROR HANDLER

--- UTIL/usr_util.c
+++ UTIL/usr_util.c
@@ -40,3 +40,60 @@ void check_cpu_activity()
     nrf_gpio_pin_toggle(PIN_CPU_ACTIVITY);
 }
 
+
+
+
+void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info)
+{
+     /* static error variables - in order to prevent removal by optimizers */
+    static volatile struct
+    {
+        uint32_t        fault_id;
+        uint32_t        pc;
+        uint32_t        error_info;
+        assert_info_t * p_assert_info;
+        error_info_t  * p_error_info;
+        ret_code_t      err_code;
+        uint32_t        line_num;
+        const uint8_t * p_file_name;
+    } m_error_data = {0};
+
+    // The following variable helps Keil keep the call stack visible, in addition, it can be set to
+    // 0 in the debugger to continue executing code after the error check.
+    volatile bool loop = true;
+    UNUSED_VARIABLE(loop);
+
+    m_error_data.fault_id   = id;
+    m_error_data.pc         = pc;
+    m_error_data.error_info = info;
+
+    switch (id)
+    {
+        case NRF_FAULT_ID_SDK_ASSERT:
+            m_error_data.p_assert_info = (assert_info_t *)info;
+            m_error_data.line_num      = m_error_data.p_assert_info->line_num;
+            m_error_data.p_file_name   = m_error_data.p_assert_info->p_file_name;
+            break;
+
+        case NRF_FAULT_ID_SDK_ERROR:
+            m_error_data.p_error_info = (error_info_t *)info;
+            m_error_data.err_code     = m_error_data.p_error_info->err_code;
+            m_error_data.line_num     = m_error_data.p_error_info->line_num;
+            m_error_data.p_file_name  = m_error_data.p_error_info->p_file_name;
+            break;
+    }
+
+    UNUSED_VARIABLE(m_error_data);
+
+    // If printing is disrupted, remove the irq calls, or set the loop variable to 0 in the debugger.
+    __disable_irq();
+
+    NRF_LOG_INFO("err_code: %d", m_error_data.err_code);
+    NRF_LOG_INFO("line_num: %d", m_error_data.line_num);
+    NRF_LOG_INFO("p_file_name: %s", m_error_data.p_file_name);
+    NRF_LOG_FINAL_FLUSH();
+
+    while (loop);
+
+    __enable_irq();
+}
