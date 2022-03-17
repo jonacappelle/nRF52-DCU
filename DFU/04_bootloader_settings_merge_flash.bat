5b962a886acdca563b81c350ad98b57c6f2c444c
jonacappelle <jonacappelle@hotmail.com>
2022-03-17 15:26:29 +0100

    DFU dcu

--- DFU/04_bootloader_settings_merge_flash.bat
+++ DFU/04_bootloader_settings_merge_flash.bat
@@ -19,21 +19,21 @@ ECHO "mergehex was not found in PATH, please install using windows installer fro
 goto :end
 )
 
-SET APPLICATION_HEX=app.hex
+SET APPLICATION_HEX=nrf52832_xxaa.hex
 
 echo "## Looking to make sure %APPLICATION_HEX% is present in folder"
 if not exist %APPLICATION_HEX% (
-echo "#### app.hex file does not exist! Please copy a application .hex file into the folder, rename it, and try again!"
+echo "#### nrf52832_xxaa.hex file does not exist! Please copy a application .hex file into the folder, rename it, and try again!"
 goto :end
 )
 echo.
 
-echo "## Creating bootloader settings based on app.hex"
-nrfutil settings generate --family NRF52 --application app.hex --application-version 1 --bootloader-version 1 --bl-settings-version 1 settings.hex
+echo "## Creating bootloader settings based on nrf52832_xxaa.hex"
+nrfutil settings generate --family NRF52 --application nrf52832_xxaa.hex --application-version 1 --bootloader-version 1 --bl-settings-version 1 settings.hex
 echo.
 
-echo "## Merging bootloader settings and app.hex into merged.hex"
-mergehex -m app.hex settings.hex -o merged.hex
+echo "## Merging bootloader settings and nrf52832_xxaa.hex into merged.hex"
+mergehex -m nrf52832_xxaa.hex settings.hex -o merged.hex
 echo.
 
 nrfjprog --program merged.hex --sectorerase
